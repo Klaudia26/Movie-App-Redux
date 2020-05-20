08c1516be44b5775c3cb85030d8a6a0a9d2a96f0
Klaudia <klaudia.krzywda@gmail.com>
2020-05-20 06:55:09 +0100

    reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,12 @@
 import { combineReducers } from 'redux';
+import moviesReducer from './moviesReducer';
+import topMoviesReducer from './topMoviesReducer';
+import popularMoviesReducer from './popularMoviesReducer';
+import upcomingMoviesReducer from './upcomingMoviesReducer';
 
-export default combineReducers(() => {});
+export default combineReducers({
+  movies: moviesReducer,
+  topMovies: topMoviesReducer,
+  popularMovies: popularMoviesReducer,
+  upcomingMovies: upcomingMoviesReducer,
+});
