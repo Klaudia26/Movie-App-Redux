0ad6dfb4025a109b5de2fb26e0c23bdb5cb0e442
Klaudia <klaudia.krzywda@gmail.com>
2020-05-20 06:54:57 +0100

    add TopBar

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSearchMovies } from '../actions';
 import './TopBar.scss';
 
 class TopBar extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+    if (e.target.value) {
+      this.props.fetchSearchMovies(e.target.value);
+    }
+  };
   render() {
-    return <div>TopBar</div>;
+    return (
+      <div className="topBar">
+        <div className="container">
+          <div>logo</div>
+          <div className="serach">
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>navigation</div>
+        </div>
+      </div>
+    );
   }
 }
-export default TopBar;
+
+const mapStateToProps = (state) => {
+  console.log(state.movies);
+  return {
+    movies: state.movies,
+  };
+};
+
+export default connect(mapStateToProps, { fetchSearchMovies })(TopBar);
