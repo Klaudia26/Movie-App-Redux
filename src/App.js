3ad557de868697f1700b0d33d87951158aea075b
Klaudia <klaudia.krzywda@gmail.com>
2020-05-21 13:10:05 +0100

    add new pages

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import SideBarFilters from './components/SideBarFilters/SideBarFilters';
 import MainPage from './components/MovieViews/MainPage';
 import SideBarNews from './components/SideBarNews/SideBarNews';
 import './MainStyle.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MoviesPage from './components/MovieViews/MoviesPage';
+import TvShowsPage from './components/MovieViews/TvShowsPage';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <TopBar />
-        <SideBarFilters />
-        <MainPage />
-        <SideBarNews />
+        <>
+          <TopBar />
+          <SideBarFilters />
+          <Route exact path="/" component={MainPage} />
+          <Route path="/movies" component={MoviesPage} />
+          <Route path="/tvshows" component={TvShowsPage} />
+          <SideBarNews />
+        </>
       </Router>
     );
   }
