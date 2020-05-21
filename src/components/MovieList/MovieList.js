8354e4b9dedab80f41f56616aea693087865e171
Klaudia <klaudia.krzywda@gmail.com>
2020-05-21 13:09:25 +0100

    refector style

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -3,14 +3,13 @@ import MovieItem from '../MovieItem/MovieItem';
 import './MovieList.scss';
 
 const MovieList = ({ movies, heading }) => {
-  console.log('props', movies);
   if (Array.isArray(movies)) {
     return (
       <div>
         <h2>{heading}</h2>
         <ul className="movies-list">
           {movies.map((movie) => {
-            return <MovieItem movie={movie} />;
+            return <MovieItem movie={movie} key={movie.id} />;
           })}
         </ul>
       </div>
@@ -23,14 +22,14 @@ const MovieList = ({ movies, heading }) => {
         <h2>Top Movies</h2>
         <ul className="movies-list slider">
           {topMovies.map((movie) => {
-            return <MovieItem movie={movie} />;
+            return <MovieItem movie={movie} key={movie.id} />;
           })}
         </ul>
 
         <h2>Popular Movies</h2>
         <ul className="movies-list slider">
           {popularMovies.map((movie) => {
-            return <MovieItem movie={movie} />;
+            return <MovieItem movie={movie} key={movie.id} />;
           })}
         </ul>
       </div>
