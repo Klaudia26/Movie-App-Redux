696025c1afe5677301293a98142ff3d852763cb9
Klaudia <klaudia.krzywda@gmail.com>
2020-05-27 06:42:50 +0100

    implement filters in sideBarFilters

--- src/components/SideBarFilters/SideBarFilters.js
+++ src/components/SideBarFilters/SideBarFilters.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchGenreMovie } from '../../actions/APIActions';
+import { toggleGenreFilter } from '../../actions/FiltersActions';
 import './SideBarFilters.scss';
 
 class SideBarFilters extends Component {
@@ -13,7 +14,18 @@ class SideBarFilters extends Component {
         <h3>Genre</h3>
         <ul>
           {this.props.genreMovie.map((genre) => {
-            return <li key={genre.id}>{genre.name}</li>;
+            return (
+              <li key={genre.id}>
+                <label>
+                  <input
+                    type="checkbox"
+                    onChange={() => this.props.toggleGenreFilter(genre.id)}
+                    value={this.props.activeFilters.genres.includes(genre.id)}
+                  />
+                </label>
+                {genre.name}
+              </li>
+            );
           })}
         </ul>
       </div>
@@ -22,9 +34,12 @@ class SideBarFilters extends Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log('genre', state);
   return {
+    activeFilters: state.activeFilters,
     genreMovie: state.dataApi.genreMovie,
+    allMovies: state.dataApi.allMovies,
   };
 };
-export default connect(mapStateToProps, { fetchGenreMovie })(SideBarFilters);
+export default connect(mapStateToProps, { fetchGenreMovie, toggleGenreFilter })(
+  SideBarFilters
+);
