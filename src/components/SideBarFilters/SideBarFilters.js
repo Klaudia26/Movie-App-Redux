53bd05a128552ec5158f0f648a9170723e4ec40f
Klaudia <klaudia.krzywda@gmail.com>
2020-05-28 10:45:07 +0100

    refactor sideBarFilters

--- src/components/SideBarFilters/SideBarFilters.js
+++ src/components/SideBarFilters/SideBarFilters.js
@@ -6,6 +6,7 @@ import {
   toggleLanguagesFilter,
 } from '../../actions/FiltersActions';
 import './SideBarFilters.scss';
+import FilterList from './FilterList';
 
 class SideBarFilters extends Component {
   componentDidMount() {
@@ -14,64 +15,21 @@ class SideBarFilters extends Component {
   }
   render() {
     return (
-      <>
-        <div className="sideBarFilters">
-          <div className="filters-list">
-            <h2 className="heading">Genre</h2>
-            <div className="scroll-wrapper">
-              <ul className="list">
-                {this.props.genreMovie.map((genre) => {
-                  return (
-                    <li key={genre.id} className="list__item">
-                      <label className="label">
-                        <input
-                          type="checkbox"
-                          onChange={() =>
-                            this.props.toggleGenreFilter(genre.id)
-                          }
-                          value={this.props.activeFilters.genres.includes(
-                            genre.id
-                          )}
-                          className="input"
-                        />
-                        <span className="checkmark" />
-                        {genre.name}
-                      </label>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-          <div className="filters-list">
-            <h2 className="heading">Languages</h2>
-            <div className="scroll-wrapper">
-              <ul className="list">
-                {this.props.languagesMovie.map((language) => {
-                  return (
-                    <li key={language.id} className="list__item">
-                      <label className="label">
-                        <input
-                          type="checkbox"
-                          onChange={() =>
-                            this.props.toggleLanguagesFilter(language.iso_639_1)
-                          }
-                          value={this.props.activeFilters.languages.includes(
-                            language.id
-                          )}
-                          className="input"
-                        />
-                        <span className="checkmark" />
-                        {language.english_name}
-                      </label>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </>
+      <div className="sideBarFilters">
+        <FilterList
+          heading={'Genre'}
+          filters={this.props.genreMovie}
+          toggleFilter={this.props.toggleGenreFilter}
+          activeFilters={this.props.activeFilters.genres}
+        />
+
+        <FilterList
+          heading={'Languages'}
+          filters={this.props.languagesMovie}
+          toggleFilter={this.props.toggleLanguagesFilter}
+          activeFilters={this.props.activeFilters.languages}
+        />
+      </div>
     );
   }
 }
